97bd22115654521bf0534f2f3c02735aa5958416
PedroLezama <113399945+PedroLezama@users.noreply.github.com>
2023-02-22 19:26:08 -0600

    Impresión del tablero de juego Gato

---
+++ Gato.java
@@ -0,0 +1,11 @@
+public class Gato{
+	public static void main(String[] args){
+	System.out.println("-------------");
+	System.out.println("| X | O | X |");
+	System.out.println("|   | O | O |");
+	System.out.println("| X |   |   |");
+	System.out.println("-------------");
+
+}
+
+}
