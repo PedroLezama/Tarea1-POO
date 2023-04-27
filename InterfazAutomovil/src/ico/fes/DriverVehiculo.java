226b41bf56024c3810aca3860461470ffe8984dd
PedroLezama <113399945+PedroLezama@users.noreply.github.com>
2023-04-26 23:30:03 -0600

    Interfaces en Java

---
+++ InterfazAutomovil/src/ico/fes/DriverVehiculo.java
@@ -0,0 +1,21 @@
+package ico.fes;
+
+public class DriverVehiculo {
+    public static void main(String[] args) {
+        Camion camion = new Camion("Diesel",3);
+        System.out.println(camion);
+        System.out.println(camion.encender());
+
+        camion.avanzar();
+        camion.frenar();
+        System.out.println(camion.apagar());
+        System.out.println("--------Motocicleta---------");
+        Motocicleta moto = new Motocicleta(150,"Yamaha");
+        System.out.println(moto);
+        System.out.println(moto.encender());
+        moto.avanzar();
+        moto.hacerCaballito();
+        moto.frenar();
+        System.out.println(moto.apagar());
+    }
+}
