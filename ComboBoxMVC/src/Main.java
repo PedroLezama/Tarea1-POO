3b767a403114f78e35e32fe9793260e5c3c549ea
PedroLezama <113399945+PedroLezama@users.noreply.github.com>
2023-05-11 19:33:46 -0600

    Implementación MVC

---
+++ ComboBoxMVC/src/Main.java
@@ -0,0 +1,8 @@
+import ico.fes.Ventana;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Hola");
+    }
+}
